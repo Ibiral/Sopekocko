15ad6feff5accab6bb2b0ff4b9e28970bff02b29
Ibiral <tnt_djurdjura@hotmail.fr>
2021-03-24 16:36:50 +0100

    Project init

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+// Importation des modules
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+// Importation des routes
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+
+// Application express
+const app = express();
+
+// Connexion à MongoDB
+mongoose.connect('mongodb+srv://Laurel:Openclassrooms@cluster0.y8hyf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Gestion des erreurs CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// Gestion de JSON
+app.use(express.json());
+
+// Chemin de sauvegarde des photos dans le backend
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Les Routes
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ backend/routes/sauces.js
@@ -0,0 +1,25 @@
+// Déclaration du Router
+const express = require('express');
+const router = express.Router();
+
+// Importation des Middlewares
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+// Importation du Controller
+const saucesCtrl = require('../controllers/sauces');
+
+// Enregitrer des sauces dans la base de données
+router.post('/', auth, multer, saucesCtrl.createSauce);
+// Mettre à jour une sauce 
+router.put('/:id', auth, saucesCtrl.modifySauce);
+// Supprimer une sauces
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+// Récupérer de la liste des sauces
+router.get('/', auth, saucesCtrl.getAllSauces);
+//Récupérer une sauce spécifique
+router.get('/:id', auth, saucesCtrl.getOneSauce);
+
+
+// Exportation du Router
+module.exports = router;

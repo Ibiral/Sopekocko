58d37696e05101b8b9223947fb788f60c9b55063
Ibiral <tnt_djurdjura@hotmail.fr>
2021-04-01 17:43:00 +0200

    Implémentation du masquage des e-mails

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -8,7 +8,7 @@ exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10) 
         .then(hash => {
             const user = new User({
-                email: req.body.email,
+                email: mask(req.body.email),
                 password: hash
             });
             user.save()
@@ -21,7 +21,7 @@ exports.signup = (req, res, next) => {
 //Valisation de connexion de l'utilisateur
 exports.login = (req, res, next) => {
     //Chercher si l'e-mail utilisateur est présent dans la BDD
-    User.findOne({ email: req.body.email })
+    User.findOne({ email: mask(req.body.email) })
         .then(user => {
             if (!user) {
                 return res.status(401).json({ error: 'Utilisateur non trouvé !' });
@@ -44,3 +44,33 @@ exports.login = (req, res, next) => {
         })
         .catch(error => res.status(500).json({ error }));
 };
+
+const gap = 800;
+function mask(email, reveal=false) {
+  let newMail = "";
+  let arobase = false;
+  for (let i = 0; i < email.length; i++) {
+    if (email[i] === "@") {
+      newMail += "@";
+      arobase = true;
+      continue;
+    }
+    if (arobase && email[i] === "."){
+      newMail += email.slice(i);
+      break;
+    }
+    if (reveal) newMail += String.fromCharCode(email.charCodeAt(i) - gap);
+    else        newMail += String.fromCharCode(email.charCodeAt(i) + gap);
+  }
+  return newMail;
+}
+
+
+
+
+// function unmask(email){
+//   return mask(email, true)
+// }
+
+// console.log(mask("test@gmail.com"));
+// console.log(unmask("Δ΅ΓΔ@·΍΁ΉΌ.com"));
